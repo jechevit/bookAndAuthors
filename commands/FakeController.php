57a4105062d93e1877fdb75f1c3d155b64dd9e3b
ec <ec@investprojects.info>
2024-08-23 15:37:41 +0300

    refactor

--- commands/FakeController.php
+++ commands/FakeController.php
@@ -29,23 +29,14 @@ class FakeController extends Controller
     public function actionCreateBooks()
     {
         $generator = \Faker\Factory::create('ru_RU');
-//
         $textFaker = new Text($generator);
 
         $authors = Author::find()->indexBy('id')
-            ->limit(200)
-        ;
+            ->limit(200);
 
         $form = new BookForm();
         foreach ($authors->batch(50) as $batch) {
-//            $form = new BookForm();
-            echo 'get batch - ' . round(memory_get_usage() / 1024 / 1024, 2) . ' MB' . PHP_EOL;
-            echo '-------------------'. PHP_EOL;
             foreach ($batch as $key => $author) {
-//                $generator = \Faker\Factory::create('ru_RU');
-
-//                $textFaker = new Text($generator);
-                $generate1 = round(memory_get_usage() / 1024 / 1024, 2);
                 $data = [
                     'name' => $generator->realText(rand(14, 30)),
                     'description' => $textFaker->realText(rand(100, 200)),
@@ -53,8 +44,6 @@ class FakeController extends Controller
                     'isbn' => $generator->isbn13(),
                 ];
 
-                $generate = round(memory_get_usage() / 1024 / 1024, 2);
-//                echo 'generate data - ' .  round(memory_get_usage() / 1024 / 1024, 2) . ' MB' . PHP_EOL;
                 $authors = [$author];
                 if ($key % 3 === 0) {
                     $randomAuthor = $batch[array_rand($batch, 1)];
@@ -62,31 +51,13 @@ class FakeController extends Controller
                         $authors[] = $randomAuthor;
                     }
                 }
-                $random = round(memory_get_usage() / 1024 / 1024, 2);
-//                echo 'random actions - ' .  round(memory_get_usage() / 1024 / 1024, 2) . ' MB' . PHP_EOL;
-                if ($form->load($data, '')
-//                    && $form->validate()
-                ) {
-                    $loadAndValidate = round(memory_get_usage() / 1024 / 1024, 2);
-//                    echo 'load and validate - ' .   round(memory_get_usage() / 1024 / 1024, 2) . ' MB' . PHP_EOL;
+                if ($form->load($data, '') && $form->validate()) {
                     $this->bookService->create($form, $authors);
-                    $create = round(memory_get_usage() / 1024 / 1024, 2);
-
-//                    echo 'create - ' .   round(memory_get_usage() / 1024 / 1024, 2) . ' MB' . PHP_EOL;
                 }
-
-                echo 'generate1 data - ' .  $generate1 . ' MB;'.
-                    'generate data - ' .  $generate . ' MB;'
-                    . ' random actions - ' . $random  . ' MB;'
-                    . ' load and validate -  ' . ($loadAndValidate ?? '-')  . ' MB;'
-                    .' create - ' .   ($create ?? '-') . ' MB' . PHP_EOL;
-                echo '#######################'. PHP_EOL;
                 unset($data);
                 unset($authors);
 
                 gc_collect_cycles();
-//                unset($textFaker);
-//                unset($generator);
             }
         }
     }
